57a30d2c8f0aeeb5504b42cb4ad9e895c3b4f651
JpakMedia <Jpak@JpakMedia.com>
2014-08-09 22:13:34 -0400

    Photo Gallery CPT, "Fluid" section page, Temp. Photo Gallery Single.

--- plugins/ucfbands-custom/ucfbands-custom.php
+++ plugins/ucfbands-custom/ucfbands-custom.php
@@ -935,6 +935,63 @@ add_action( 'init', 'ucfbands_pep_band', 0 );
 
 
 
+
+//------------------------//
+// REGISTER PHOTO GALLERY //
+//------------------------//
+function ucfbands_photo_gallery() {
+
+	$labels = array(
+		'name'                => _x( 'Photo Galleries', 'Post Type General Name', 'text_domain' ),
+		'singular_name'       => _x( 'Photo Gallery', 'Post Type Singular Name', 'text_domain' ),
+		'menu_name'           => __( 'Photo Galleries', 'text_domain' ),
+		'parent_item_colon'   => __( 'Parent Gallery', 'text_domain' ),
+		'all_items'           => __( 'All Galleries', 'text_domain' ),
+		'view_item'           => __( 'View Gallery', 'text_domain' ),
+		'add_new_item'        => __( 'Add New Gallery', 'text_domain' ),
+		'add_new'             => __( 'Add New', 'text_domain' ),
+		'edit_item'           => __( 'Edit Gallery', 'text_domain' ),
+		'update_item'         => __( 'Update Gallery', 'text_domain' ),
+		'search_items'        => __( 'Search Galleries', 'text_domain' ),
+		'not_found'           => __( 'Not Found', 'text_domain' ),
+		'not_found_in_trash'  => __( 'Not found in Trash', 'text_domain' ),
+	);
+	$rewrite = array(
+		'slug'                => 'gallery',
+		'with_front'          => true,
+		'pages'               => true,
+		'feeds'               => false,
+	);
+	$args = array(
+		'label'               => __( 'ucfbands_photo_gallery', 'text_domain' ),
+		'description'         => __( 'Basically a container for a Ess. Grid Custom Gallery', 'text_domain' ),
+		'labels'              => $labels,
+		'supports'            => array( 'title', 'editor', 'thumbnail', ),
+		'taxonomies'          => array( 'category' ),
+		'hierarchical'        => false,
+		'public'              => true,
+		'show_ui'             => true,
+		'show_in_menu'        => true,
+		'show_in_nav_menus'   => true,
+		'show_in_admin_bar'   => true,
+		'menu_position'       => 5,
+		'menu_icon'           => 'dashicons-format-gallery',
+		'can_export'          => true,
+		'has_archive'         => true,
+		'exclude_from_search' => false,
+		'publicly_queryable'  => true,
+		'rewrite'             => $rewrite,
+		'capability_type'     => 'page',
+	);
+	register_post_type( 'photo_gallery', $args );
+
+}
+
+// Hook into the 'init' action
+add_action( 'init', 'ucfbands_photo_gallery', 0 );
+
+
+
 //-------------------//
 // CUSTOM META BOXES //
 //-------------------//
---
+++ themes/ucfbands/single-photo_gallery.php
@@ -0,0 +1,137 @@
+<?php
+/*
+Template Name: Section Page (Fluid)
+*/
+?>
+
+<?php get_header(); ?>
+
+			<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+            
+            <!--// FIX AFFIX //-->
+            <div class="top-fixed">
+            
+            	
+                <?php 
+				
+					// Parent Post
+					$page_parents = get_post_ancestors( $post );
+					$parent_id = $page_parents[0];
+					
+					$parent_post = get_post( $parent_id );
+					$parent_slug = $parent_post->post_name;
+					
+										
+					// Get Parent (Dashboard) Page Data
+					$parent_title = get_the_title( $post->post_parent );
+					
+				
+					// Get Featured Image Link // 
+					$single_featured_image = wp_get_attachment_url( get_post_thumbnail_id( $parent_id ));
+					
+					
+					// GET GALLERY CATEGORY SLUG
+					$category = get_the_category($post->ID);
+					$category = $category[0]->slug;
+
+						
+				?>
+            
+                
+                <!--// SECTION TITLE //-->
+                <div class="section-title <?php echo $category; ?>" style="padding: 40px; padding-left: 0px; padding-right: 0px;">
+                <?php /* <div class="section-title <?php echo $category; ?>" style="background-image: url('<?php echo $single_featured_image ?>');"> */ ?>
+                
+                    <a href="<?php echo get_permalink( $parent_id ); ?>"><h1><?php echo $parent_title //the_title(); ?></h1></a>
+                
+                </div><!-- /.section-title -->
+                
+                
+                
+                <!--// SECTION MENU //-->
+				<?php
+                	
+										
+					// Menu Options
+					$subnav_options = array(
+						//'theme_location'  => get_the_title(),
+						'menu'            => $category,
+						'container'       => 'nav',
+						'container_class' => 'sub',
+						'menu_class'      => 'sub',
+						'menu_id'         => '',
+						'echo'            => true,
+						'fallback_cb'     => 'wp_page_menu',
+						'depth'           => 2,
+						'walker'          => new wp_bootstrap_navwalker()
+					);
+
+					
+					// Output Menu
+					wp_nav_menu( $subnav_options );
+                
+                ?>                
+                
+                                    
+            </div><!-- /.top-fixed -->
+
+            
+            <!--// PAGE CONTENT //-->
+            <div id="page-content-fluid" class="<?php echo $parent_slug; ?>">
+            			
+                                    
+                    <?php 
+					
+						if( get_the_content() == '' )
+						{
+							echo '<div class="row">';
+
+							echo '<div class="col-lg-3"><div class="block block-featured"><h2><i class="fa fa-calendar"></i> Coming Soon!</h2></div></div>';
+							
+							echo '</div><!-- /.row -->';
+						}
+						else
+							the_content(); 
+					
+					?>
+
+                    <?php wp_link_pages(); ?>
+                
+                
+                 
+           		
+				
+				<?php endwhile; ?>		
+                
+                
+                
+                <!--// IF NO CONTENT FOUND //-->
+          		<div class="row">
+                
+                    
+                    <?php else : ?>
+                    
+                    <article id="post-not-found">
+                        
+                        <header>
+                            <h1><?php _e("Not Found", "wpbootstrap"); ?></h1>
+                        </header>
+                        
+                        <section class="post_content">
+                            <p><?php _e("Sorry, but the requested resource was not found on this site.", "wpbootstrap"); ?></p>
+                        </section>
+                        
+                        <footer>
+                        </footer>
+                    </article>
+                    
+                    <?php endif; ?>
+
+                
+            	</div><!-- /.row -->
+                
+            </div><!-- /#page-content -->
+			
+
+<?php get_footer(); ?>
